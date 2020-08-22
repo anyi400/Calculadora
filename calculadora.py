e1afa7523bb7e3c10142345f3b4ad0e0e3101c63
anyi400 <anyiljc11@gmail.com>
2020-08-21 20:24:19 -0400

    calculadora

---
+++ calculadora.py
@@ -0,0 +1,61 @@
+def ingreso():
+
+
+global a, b
+a = int(raw_input("ingrese el prier valor\n"))
+b = int(raw_input("ingrese primer valor\n"))
+
+
+def suma()
+
+
+print "el valor es:", a+b
+raw_input()
+
+
+def resta()
+
+
+print "el valor es", a-b
+raw_input
+
+
+def multiplicacion()
+
+
+print "el valor es:", a*b
+raw_input
+
+
+def division()
+
+
+if (b == 0):
+    print "no se divide en 0:"
+    raw_input()
+else:
+    print "el resultado es:", a/b
+    raw_input
+
+
+while (true)
+print " primera operaciom suma"
+print "segunda operacion resta"
+print "tercera operacion multiplicacion"
+print "cuarta operacion division"
+opcion = int(raw_input("ingrese a opcion como operacion\n"))
+if (opcion == 1)
+ingreso()
+suma()
+
+elif(opcion == 2)
+ingreso()
+resta()
+
+elif (opcion == 3)
+ingreso()
+multiplicacion()
+
+elif (opcion == 4)
+ingreso()
+division()
