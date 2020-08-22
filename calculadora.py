d23a1111825ce2ce538480c0dc2ae9d402b34bf9
anyi400 <anyiljc11@gmail.com>
2020-08-21 21:16:19 -0400

    calculadoraa

--- calculadora.py
+++ calculadora.py
@@ -1,61 +1,62 @@
 def ingreso():
+    global a, b
+    a = int(raw_input("ingrese el primer valor"\n))
+    b = int(raw_input("ingrese el primer valor"\n))
 
 
-global a, b
-a = int(raw_input("ingrese el prier valor\n"))
-b = int(raw_input("ingrese primer valor\n"))
-
-
-def suma()
+def suma();
 
 
 print "el valor es:", a+b
 raw_input()
 
 
-def resta()
+def resta();
 
 
-print "el valor es", a-b
+print "el valor es ", a-b
+raw_input()
+
+
+def multiplicacion();
+
+
+print "el resultado es;", a*b
+raw_input()
+
+
+def division();
+
+
+if (b == 0)
+print "no se puede dividir en 0"
 raw_input
 
 
-def multiplicacion()
-
-
-print "el valor es:", a*b
-raw_input
-
-
-def division()
-
-
-if (b == 0):
-    print "no se divide en 0:"
-    raw_input()
 else:
-    print "el resultado es:", a/b
+    print "el valor es:", a/b
     raw_input
 
 
-while (true)
-print " primera operaciom suma"
-print "segunda operacion resta"
-print "tercera operacion multiplicacion"
-print "cuarta operacion division"
-opcion = int(raw_input("ingrese a opcion como operacion\n"))
-if (opcion == 1)
-ingreso()
-suma()
+while (true):
+    print "primer operacion suma"
+      print "segunda operacion resta"
+        print "tercera operacion multiplicacion"
+          print "primer operacion division
 
-elif(opcion == 2)
-ingreso()
-resta()
+        opcion = int(raw_imput(ingrese la primera opcion\n))
+         if (opcion == 1);
+         ingreso()
+         suma()
 
-elif (opcion == 3)
-ingreso()
-multiplicacion()
+    elif (opcion == 2);
+          ingreso()
+          resta()
 
-elif (opcion == 4)
-ingreso()
-division()
+   elif (opcion==3);
+          ingreso()
+          multiplicacion()
+
+    elif (opcion==4);
+          ingreso()
+          division ()
